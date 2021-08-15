6aa78117ccd5ba3d62dc81c7b4f0209aea1dc2e7
Jojojojojojo <jojojojojojo@MacBook-Pro-de-Amalia-1.home>
2021-08-15 19:17:58 +0200

    first and final push

---
+++ normivite.sh
@@ -0,0 +1,6 @@
+while [ 1 ]
+do
+	~/norminette/.env/bin/norminette $1
+	sleep 1;
+done
+
---
+++ setup.sh
@@ -0,0 +1,7 @@
+cp normivite.sh ~/
+chmod 777 ~/normivite.sh
+echo 'alias normivite="~/normivite.sh ."' >> ~/.zshrc
+echo "To use normivite, you need to have the norminette v3, you can find theinstaller for MacOS here : https://github.com/JCluzet/Install_Normv3_onMAC\n\n"
+sleep 2
+echo "Normivite setup finish, please close and open your terminal" 
+echo "Use normivite with the command > /normivite"
